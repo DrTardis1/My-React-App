952e5ddbc1167a70a6f21c7b23dbdac37c492420
Bryce Lynch <techbryce@hotmail.com>
2021-02-05 23:03:02 +1100

    V1

--- src/App.js
+++ src/App.js
@@ -1,30 +1,48 @@
+// import React from 'react';
+
+// class App extends React.Component{
+
+//   constructor(){
+//     super();
+//     this.state = {
+//       txt: 'This is the state text'
+//     }
+//   }
+
+//   update(e){
+//     this.setState({txt: e.target.value});
+//   }
+
+//   render(){
+//     let txt = this.state.txt;
+//     return (
+//       <div>
+//         <Widget update={this.update.bind(this)} />
+//         <h1>{this.state.txt}</h1>
+//       </div>
+//     )
+//   }
+// }
+
+
+// const Widget = (props) =>
+// <input type="text" onChange={props.update}/>
+// export default App;
+
 import React from 'react';
 
 class App extends React.Component{
-
-  constructor(){
-    super();
-    this.state = {
-      txt: 'This is the state text'
-    }
-  }
-
-  update(e){
-    this.setState({txt: e.target.value});
-  }
-
   render(){
-    let txt = this.state.txt;
-    return (
-      <div>
-        <Widget update={this.update.bind(this)} />
-        <h1>{this.state.txt}</h1>
-      </div>
-    )
+    return <Button> <Heart/>React</Button>
   }
 }
 
+const Button = (props) =><button>{props.children}</button>
 
-const Widget = (props) =>
-<input type="text" onChange={props.update}/>
+
+class Heart extends React.Component{
+  render(){
+    return <span>&hearts;</span>
+  }
+}
 export default App;
